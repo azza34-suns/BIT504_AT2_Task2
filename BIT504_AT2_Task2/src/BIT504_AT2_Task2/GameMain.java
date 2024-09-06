720cf1bac5b26bc008e3bf3742ecddba76c72e69
azza34-suns <aaron.fern1974@gmail.com>
2024-09-07 11:49:33 +1200

    Saved changes

--- BIT504_AT2_Task2/src/BIT504_AT2_Task2/GameMain.java
+++ BIT504_AT2_Task2/src/BIT504_AT2_Task2/GameMain.java
@@ -41,7 +41,7 @@ public class GameMain extends JPanel implements MouseListener{
 	/** Constructor to setup the UI and game components on the panel */
 	public GameMain() {   
 		
-		// TODO: This JPanel fires a MouseEvent on MouseClicked so add required event listener to 'this'.          
+		// Add required event listener to 'this'.          
 	    addMouseListener(this);
 	    
 		// Setup the status bar (JLabel) to display status message       
